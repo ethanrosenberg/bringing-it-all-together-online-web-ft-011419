4d5278f4891a9a2449047976bdedfdbffaa3ed31
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-20 19:36:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,6 +3,11 @@ class Dog
   attr_accessor :name, :breed
 
   def initialize (params)
+    if params[:id]
+      @id = params[:id]
+    else
+      @id = nil
+    end
     @id ||= params[:id]
     @name = params[:name]
     @breed = params[:breed]
