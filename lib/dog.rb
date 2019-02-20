8674581f9cea363a1e2409bb13cc98617f614ea8
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-20 19:39:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,8 +14,15 @@ class Dog
     @breed = params[:breed]
   end
 
-  def create_table
-
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
   end
 
 
