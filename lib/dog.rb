9563ef95476ab6d7c3db7dbf54295751f7e2a5d2
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-20 19:47:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,7 +51,7 @@ class Dog
 
   def self.find_by_id(id)
    sql = "SELECT * FROM dogs WHERE id = ? LIMIT 1"
-   result = DB[:conn].execute(sql, id)[0]
+   result = DB[:conn].execute(sql, id)
    Dog.new(result[0], result[1], result[2])
  end
 
